e046b5552d0cc4beed80962a1485b138101d3136
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-10-11 11:33:34 -0300

    Criaçãoda api requests

---
+++ crawling-github/github_api_requests.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+GIT_USER_NAME = "daniel-hasan"
+URL_BASE = "https://api.github.com"
+AUTH_TOKEN = None
+def request_github_api(url,data={}):
+    headers = {'User-Agent': GIT_USER_NAME}
+    if AUTH_TOKEN :
+        headers["Authorization"] = 'token ' + AUTH_TOKEN
+
+    r= requests.get(url, headers=headers)
+    return json.loads(r.text or r.content)
+
+def get_repo_from_user(userName):
+    return request_github_api(URL_BASE+"/users/"+userName+"/repos")
+
+def get_user_following(userName):
+    return request_github_api(URL_BASE+"/users/"+userName+"/following")
+
+def get_user_followers(userName):
+    return request_github_api(URL_BASE+"/users/"+userName+"/followers")
+
+def get_contributors_from_repo(userOwner,repoName):
+    '''
+    Contributor: A contributor is someone from the outside not on the core development team of the project that wants to contribute some changes to a project.
+    https://github.com/CoolProp/CoolProp/wiki/Contributors-vs-Collaborators
+    '''
+    return request_github_api(URL_BASE+"/repos/"+userOwner+"/"+repoName+"/contributors")
+
+def get_collaborators_from_repo(userOwner,repoName):
+    '''
+    Requires authentication
+    Collaborator: A collaborator is someone on the core development team of the project and has commit access to the main repository of the project.
+    https://github.com/CoolProp/CoolProp/wiki/Contributors-vs-Collaborators
+    '''
+    return request_github_api(URL_BASE+"/repos/"+userOwner+"/"+repoName+"/collaborators")
--- crawling-github/simple_bfs_crawler.py
+++ crawling-github/simple_bfs_crawler.py
@@ -1,36 +1,12 @@
-import requests
-import json
+import github_api_requests
 
-GIT_USER_NAME = "daniel-hasan"
-URL_BASE = "https://api.github.com"
-def request_github_api(url,data={}):
-    headers = {'User-Agent': GIT_USER_NAME}
-
-    r= requests.get(url, headers=headers)
-    return json.loads(r.text or r.content)
-
-def get_repo_from_user(userName):
-    return request_github_api(URL_BASE+"/users/"+userName+"/repos")
-
-
-def get_contributors_from_repo(userOwner,repoName):
-    '''
-    Contributor: A contributor is someone from the outside not on the core development team of the project that wants to contribute some changes to a project.
-    https://github.com/CoolProp/CoolProp/wiki/Contributors-vs-Collaborators
-    '''
-    return request_github_api(URL_BASE+"/repos/"+userOwner+"/"+repoName+"/contributors")
-
-def get_collaborators_from_repo(userOwner,repoName):
-    '''
-    Requires authentication
-    Collaborator: A collaborator is someone on the core development team of the project and has commit access to the main repository of the project.
-    https://github.com/CoolProp/CoolProp/wiki/Contributors-vs-Collaborators
-    '''
-    return request_github_api(URL_BASE+"/repos/"+userOwner+"/"+repoName+"/collaborators")
-
-
+class GithubUserCrawled():
+    def __init__(self,url,level):
+        self.url = url
+        self.level = level
 
 
 
 if __name__ == "__main__":
-  print(get_collaborators_from_repo("daniel-hasan","cefet-web"))
+
+    print(get_user_followers("daniel-hasan"))
