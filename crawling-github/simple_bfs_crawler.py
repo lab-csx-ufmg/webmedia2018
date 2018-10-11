082b8d1df9cbb380db276485dc2765aa00166404
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-10-11 11:53:29 -0300

    BFS

--- crawling-github/simple_bfs_crawler.py
+++ crawling-github/simple_bfs_crawler.py
@@ -1,9 +1,33 @@
 import github_api_requests
 
 class GithubUserCrawled():
-    def __init__(self,url,level):
-        self.url = url
+    def __init__(self,name,level):
+        self.name = name
         self.level = level
+
+class ApiBFSCrawler():
+    def __init__(self,userSeeds):
+        self.userSeeds = userSeeds
+        self.queue = []
+
+    def perform_crawl(maxLevel,output):
+        '''
+            Crawl using breath first search add all the following users of
+            each user crawled respecting the depth level of the crawling
+        '''
+        #add the seed to the queue
+        [self.queue.append(GithubUserCrawled(user,0)) for user in self.userSeeds]
+
+        with open(output,"w") as file:
+            while len(self.queue)>0:
+                #dequeue de first element
+                user = self.queue.pop(0)
+                #save its repositories urls
+                for repo in get_repo_from_user(user):
+                    
+
+                #add the following users
+                get_user_following(user.name)
 
 
 
