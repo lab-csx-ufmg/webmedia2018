ee6f42124ffc7461c1d756c43f8afc7fdd7d43db
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-10-11 12:51:40 -0300

    BFS implementation

--- crawling-github/simple_bfs_crawler.py
+++ crawling-github/simple_bfs_crawler.py
@@ -1,10 +1,11 @@
-import github_api_requests
+from github_api_requests import *
 
 class GithubUserCrawled():
     def __init__(self,name,level):
         self.name = name
         self.level = level
-
+    def __str__(self):
+        return "Name: "+self.name+" level: "+str(level)
 class ApiBFSCrawler():
     def __init__(self,userSeeds):
         self.userSeeds = userSeeds
@@ -22,15 +23,18 @@ class ApiBFSCrawler():
             while len(self.queue)>0:
                 #dequeue de first element
                 user = self.queue.pop(0)
-                #save its repositories urls
+                print("Crawling user: "+user+" Queue size: "+str(len(self.queue)))
+                #save its repositories clone url
                 for repo in get_repo_from_user(user):
-                    
+                    file.write(repo["name"],repo["clone_url"])
 
-                #add the following users
-                get_user_following(user.name)
+                #add the following users in the queue
+                for userFollowing in get_user_following(user.name):
+                    if(user.level < maxLevel):
+                        self.queue.append(GithubUserCrawled(userFollowing['login'],user.level+1)
 
 
 
 if __name__ == "__main__":
 
-    print(get_user_followers("daniel-hasan"))
+    print(get_user_following("fegemo")[0])
