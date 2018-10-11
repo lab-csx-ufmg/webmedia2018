83522786a504f631aeebc6fbb3cb08133cbe3e60
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-10-11 11:02:21 -0300

    Collaborators

--- crawling-github/simple_bfs_crawler.py
+++ crawling-github/simple_bfs_crawler.py
@@ -12,11 +12,25 @@ def request_github_api(url,data={}):
 def get_repo_from_user(userName):
     return request_github_api(URL_BASE+"/users/"+userName+"/repos")
 
+
 def get_contributors_from_repo(userOwner,repoName):
+    '''
+    Contributor: A contributor is someone from the outside not on the core development team of the project that wants to contribute some changes to a project.
+    https://github.com/CoolProp/CoolProp/wiki/Contributors-vs-Collaborators
+    '''
     return request_github_api(URL_BASE+"/repos/"+userOwner+"/"+repoName+"/contributors")
+
+def get_collaborators_from_repo(userOwner,repoName):
+    '''
+    Requires authentication
+    Collaborator: A collaborator is someone on the core development team of the project and has commit access to the main repository of the project.
+    https://github.com/CoolProp/CoolProp/wiki/Contributors-vs-Collaborators
+    '''
+    return request_github_api(URL_BASE+"/repos/"+userOwner+"/"+repoName+"/collaborators")
+
 
 
 
 
 if __name__ == "__main__":
-  print(get_contributors_from_repo("daniel-hasan","cefet-web-coral55"))
+  print(get_collaborators_from_repo("daniel-hasan","cefet-web"))
