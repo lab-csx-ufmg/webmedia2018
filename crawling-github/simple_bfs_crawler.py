83d2b6be703bce775f9c45ece5c730c2708464cf
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-10-12 10:23:50 -0300

    BFS crawler

--- crawling-github/simple_bfs_crawler.py
+++ crawling-github/simple_bfs_crawler.py
@@ -1,15 +1,18 @@
 from github_api_requests import *
+from collections import deque
 
-class GithubUserCrawled():
-    def __init__(self,name,level):
-        self.name = name
+class GithubRepoCrawled():
+    def __init__(self,user,repoName,level):
+        self.user = user
+        self.repoName =repoName
         self.level = level
     def __str__(self):
         return "Name: "+self.name+" level: "+str(level)
+
 class ApiBFSCrawler():
     def __init__(self,userSeeds):
-        self.userSeeds = userSeeds
-        self.queue = []
+        self.repoSeeds = repoSeeds
+        self.queue = deque([])
 
     def perform_crawl(maxLevel,output):
         '''
@@ -17,21 +20,25 @@ class ApiBFSCrawler():
             each user crawled respecting the depth level of the crawling
         '''
         #add the seed to the queue
-        [self.queue.append(GithubUserCrawled(user,0)) for user in self.userSeeds]
+        [self.queue.append(GithubRepoCrawled(user,0)) for repo in self.repoSeeds]
 
         with open(output,"w") as file:
             while len(self.queue)>0:
-                #dequeue de first element
-                user = self.queue.pop(0)
-                print("Crawling user: "+user+" Queue size: "+str(len(self.queue)))
-                #save its repositories clone url
-                for repo in get_repo_from_user(user):
-                    file.write(repo["name"],repo["clone_url"])
+                #dequeue de first repo in the queue
+                repo = self.queue.popleft()
+                print("Crawling repo: "+repo+" Queue size: "+str(len(self.queue)))
 
-                #add the following users in the queue
-                for userFollowing in get_user_following(user.name):
-                    if(user.level < maxLevel):
-                        self.queue.append(GithubUserCrawled(userFollowing['login'],user.level+1)
+                #add an edge to the graph for each contributors of this repo
+                arrContributors = get_contributors_from_repo(repo.user,repo.repoName)
+                for cont1 in arrContributors:
+                    for cont2 in arrContributors:
+                        if cont1 != cont2:
+                            file.write(cont1["login"]+","+cont2["login"])
+
+                    #add the users repo to the queue
+                    if(repo.level < maxLevel):
+                        for usrRepo in get_repo_from_user(cont1['login']):
+                            self.queue.append(GithubUserCrawled(usrRepo['owner']['login'],usrRepo['name'],user.level+1)
 
 
 
