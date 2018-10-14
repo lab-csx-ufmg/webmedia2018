63e104fd9c677613f9f77d5a29fe9eb953050fbe
Daniel Hasan Dalip <prof.daniel.hasan@gmail.com>
2018-10-14 18:07:33 -0300

    removing BFS

--- crawling-github/crawler/bfs_user_repo_contributors.py
+++ crawling-github/crawler/bfs_user_repo_contributors.py
@@ -19,34 +19,7 @@ class BFSUserRepoContributors():
                 ))
 
     def perform_crawl(self, output, maxDepth):
-        with open(output,"w") as file:
-            while len(self.queue) > 0:
-                # dequeue de first user
-                user, curDepth = self.queue.popleft()
-                print("Crawling {user}, Repos: {num_repos}, Queue size: {queue_size}".format(
-                    user=user,
-                    num_repos=len(user.repos),
-                    queue_size=len(self.queue)
-                ))
-                print("QUEUE: {queue}".format(queue=[(user.username,depth) for user,depth in self.queue]))
-
-                # for each repo of the current user
-                for repo in user.repos:
-                    print("Crawling {repo}, Contributors: {num_contributors}".format(
-                        repo=repo,
-                        num_contributors=len(repo.contributors)
-                    ))
-
-                    # write edge between all collaborators
-                    self.write_edges(file, repo)
-                    # verify if will keep walking
-                    if curDepth + 1 <= maxDepth:
-                        #add contributors to the queue
-                        for nextUser in repo.contributors:
-                            if nextUser.username not in self.discovered_users:
-                                self.queue.append((nextUser,curDepth + 1,))
-                                # ensure that user will not be added again
-                                self.discovered_users.add(nextUser.username)
+        pass
 
 if __name__ == "__main__":
     if len(sys.argv) < 3 or not int(sys.argv[2]):
